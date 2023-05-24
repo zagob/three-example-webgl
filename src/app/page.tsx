5c59f9f0f5a174389d3da29543d077bb21498155
zagob <matheusbestana@gmail.com>
2023-05-24 18:07:10 -0300

    initial project + lib three

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+
+export default function Home() {
+  return (
+    <div>
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
+  );
+}
